39ae7c021b3772cf9fdd48ad2d0303e60e628d71
Anthony Merlin <92953379+AnthonyMerlinGoHokies@users.noreply.github.com>
2022-10-20 11:14:06 -0400

    Add files via upload

---
+++ PartTimeEmployee.java
@@ -0,0 +1,54 @@
+package employees;
+
+/**
+ * Represents an average part time employee
+ *
+ * @author Anthony Merlin
+ * @version 2022.08.29
+ */
+public class PartTimeEmployee extends Employee {
+    private int hoursWorked;
+    
+    /**
+     * new PartTimeEmployee object
+     * 
+     * @param name
+     *            name of Employee
+     * @param employeeId
+     *            Employee ID of Employee
+     * @param hourlyRate
+     *            hourly pay rate of Employee
+     * @param hoursWorked
+     *            hours worked by Employee
+     */
+    public PartTimeEmployee(
+        String name,
+        int employeeId,
+        double hourlyRate,
+        int hoursWorked) {
+        super(name, employeeId, hourlyRate);
+        this.hoursWorked = hoursWorked;
+    }
+
+    /**
+     * get the number of hours worked by Employee
+     * 
+     * @return number of hours worked
+     */
+    public int getHoursWorked() {
+        return this.hoursWorked;
+    }
+
+    /**
+     * calculate the weekly pay for the Employee
+     * 
+     * @return Employee's weekly pay
+     */
+    
+    @Override
+    public double weeklyPay() {
+        return this.hoursWorked * super.getHourlyRate();
+    }
+
+}
+
---
+++ PartTimeEmployeeTest.java
@@ -0,0 +1,37 @@
+package employees;
+
+/**
+ * PartTimeEmployeeTest will test all of the methods in PartTimeEmployee to make
+ * sure that they run and perform as expected
+ *
+ * @author Anthony Merlin
+ * @version 2022.08.29
+ */
+public class PartTimeEmployeeTest extends student.TestCase {
+    private PartTimeEmployee testEmployee;
+
+
+    /**
+     * set up Employee object
+     */
+    public void setUp() {
+        this.testEmployee = new PartTimeEmployee("John Snow", 8546, 8.0, 17);
+    }
+
+
+    /**
+     * test PartTimeEmployee getHours method
+     */
+    public void testGetHoursWorked() {
+        assertEquals(this.testEmployee.getHoursWorked(), 17, 0.01);
+    }
+
+
+    /**
+     * test PartTimeEmployee weeklyPay method
+     */
+    public void testWeeklyPay() {
+        assertEquals(this.testEmployee.weeklyPay(), 17 * 8.0, 0.01);
+    }
+}
+
